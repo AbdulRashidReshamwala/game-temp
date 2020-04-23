3724182aca5ae1235c8259a3d6d68dc7f8dd9bc9
Abdul Rashid <ar1242112@gmail.com>
2020-04-23 23:10:15 +0530

    added scoring

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -2,25 +2,15 @@ import React, { useEffect, useState } from "react";
 import "./Spinner.css";
 
 export default function Spinner({ spining, result, data, speed, draw }) {
-	const [index, setIndex] = useState(0);
-	useEffect(() => {
-		const interval = setInterval(() => {
-			if (spining) {
-				setIndex((index + 1) % data.length);
-			}
-		}, speed);
-		return () => clearInterval(interval);
-	}, [spining, data, index, speed]);
+  const [index, setIndex] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (spining) {
+        setIndex((index + 1) % data.length);
+      }
+    }, speed);
+    return () => clearInterval(interval);
+  }, [spining, data, index, speed]);
 
-	return (
-		<div>
-			{spining ? (
-				<div className="roll">
-					<div id="blur">{draw(data[index])}</div>
-				</div>
-			) : (
-				<div className="roll">{draw(data[result])}</div>
-			)}
-		</div>
-	);
+  return <>{spining ? draw(data[index]) : draw(data[result])}</>;
 }
