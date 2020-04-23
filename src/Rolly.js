fc120df21b17a24db14b6436cb9969bd6ee09c7f
ubaidkolad <bhaid023@gmail.com>
2020-04-23 14:16:40 +0530

    added thick borders and slot machine number background

--- src/Rolly.js
+++ src/Rolly.js
@@ -6,235 +6,235 @@ import db from "./firbase";
 import "./Rolly.css";
 
 const images = [
-  "bell.png",
-  "cherry.png",
-  "bar.png",
-  "m.png",
-  "leaf.png",
-  "wild.png",
+	"bell.png",
+	"cherry.png",
+	"bar.png",
+	"m.png",
+	"leaf.png",
+	"wild.png",
 ];
 
 export default function Rolly() {
-  const [spinning, setSpinning] = useState(true);
-  const [status, setStatus] = useState("loading");
-  const [answers, setAnswers] = useState([]);
-  const [data, setData] = useState(Array(5).fill(Array(12).fill(99)));
-  const [index, setIndex] = useState(-1);
-  const [active, setActive] = useState(true);
+	const [spinning, setSpinning] = useState(true);
+	const [status, setStatus] = useState("loading");
+	const [answers, setAnswers] = useState([]);
+	const [data, setData] = useState(Array(5).fill(Array(12).fill(99)));
+	const [index, setIndex] = useState(-1);
+	const [active, setActive] = useState(true);
 
-  useEffect(() => {
-    if (status === "active") {
-      if (index < answers.length - 1) {
-        setTimeout(() => {
-          setSpinning(true);
-          setIndex(index + 1);
-        }, 5000);
-      }
-      setTimeout(() => {
-        setActive(false);
-      }, 5000);
-      setTimeout(() => {
-        setSpinning(false);
-        setActive(true);
-      }, 2000);
-    }
-  }, [index, answers.length, status]);
+	useEffect(() => {
+		if (status === "active") {
+			if (index < answers.length - 1) {
+				setTimeout(() => {
+					setSpinning(true);
+					setIndex(index + 1);
+				}, 5000);
+			}
+			setTimeout(() => {
+				setActive(false);
+			}, 5000);
+			setTimeout(() => {
+				setSpinning(false);
+				setActive(true);
+			}, 2000);
+		}
+	}, [index, answers.length, status]);
 
-  useEffect(() => {
-    const unsubscribe = db
-      .collection("events")
-      .doc("flowers-rolly-base-1587571664")
-      .onSnapshot(
-        (doc) => {
-          let data = doc.data();
-          console.log(data);
-          setAnswers(data.meta);
-          setStatus("active");
-          setIndex(0);
-        },
-        (err) => {
-          console.log(err);
-        }
-      );
-    return () => unsubscribe();
-  }, []);
-  const verifyPlacement = (rowId, colId, value) => {
-    if (value.row !== rowId && value.row !== 5) {
-      alert(`${value.row + 1} is active but ${rowId + 1} was clicked`);
-      return false;
-    }
-    value = value.number;
-    if (data[rowId][colId] !== 99) {
-      alert("stop should be empty");
-      return false;
-    }
-    let topElements = data[rowId].slice(0, colId);
-    topElements = topElements.filter((ele) => {
-      return ele !== 99;
-    });
-    let bottomElements = data[rowId].slice(colId + 1, 12);
-    bottomElements = bottomElements.filter((ele) => {
-      return ele !== 99;
-    });
-    for (let i = 0; i < topElements.length; i++) {
-      if (topElements[i] > value) {
-        return false;
-      }
-    }
-    for (let i = 0; i < bottomElements.length; i++) {
-      if (bottomElements[i] < value) {
-        return false;
-      }
-    }
-    return true;
-  };
+	useEffect(() => {
+		const unsubscribe = db
+			.collection("events")
+			.doc("flowers-rolly-base-1587571664")
+			.onSnapshot(
+				(doc) => {
+					let data = doc.data();
+					console.log(data);
+					setAnswers(data.meta);
+					setStatus("active");
+					setIndex(0);
+				},
+				(err) => {
+					console.log(err);
+				}
+			);
+		return () => unsubscribe();
+	}, []);
+	const verifyPlacement = (rowId, colId, value) => {
+		if (value.row !== rowId && value.row !== 5) {
+			alert(`${value.row + 1} is active but ${rowId + 1} was clicked`);
+			return false;
+		}
+		value = value.number;
+		if (data[rowId][colId] !== 99) {
+			alert("stop should be empty");
+			return false;
+		}
+		let topElements = data[rowId].slice(0, colId);
+		topElements = topElements.filter((ele) => {
+			return ele !== 99;
+		});
+		let bottomElements = data[rowId].slice(colId + 1, 12);
+		bottomElements = bottomElements.filter((ele) => {
+			return ele !== 99;
+		});
+		for (let i = 0; i < topElements.length; i++) {
+			if (topElements[i] > value) {
+				return false;
+			}
+		}
+		for (let i = 0; i < bottomElements.length; i++) {
+			if (bottomElements[i] < value) {
+				return false;
+			}
+		}
+		return true;
+	};
 
-  const putData = (colId, rowId) => {
-    if (active && verifyPlacement(rowId, colId, answers[index])) {
-      console.log("added");
-      let n = [...data];
-      let j = [...n[rowId]];
-      j[colId] = answers[index].number;
-      n[rowId] = j;
-      setData(n);
-      setActive(false);
-    }
-  };
+	const putData = (colId, rowId) => {
+		if (active && verifyPlacement(rowId, colId, answers[index])) {
+			console.log("added");
+			let n = [...data];
+			let j = [...n[rowId]];
+			j[colId] = answers[index].number;
+			n[rowId] = j;
+			setData(n);
+			setActive(false);
+		}
+	};
 
-  document.getElementById(
-    "root"
-  ).style.backgroundImage = `url("${window.location.origin}/assets/rolly/bg.png")`;
+	document.getElementById(
+		"root"
+	).style.backgroundImage = `url("${window.location.origin}/assets/rolly/bg.png")`;
 
-  return (
-    <div style={{ minHeight: "100%", textAlign: "center" }}>
-      {index > -1 ? (
-        <>
-          <Container>
-            <div className="d-lg-none d-md-none">
-              <div className="d-flex justify-content-center">
-                <Row>
-                  <Col>
-                    <Spinner
-                      spining={spinning}
-                      result={answers[index].row}
-                      speed={200}
-                      data={images}
-                      draw={(item) => (
-                        <Image
-                          fluid
-                          src={`${window.location.origin}/assets/rolly/images/${item}`}
-                          style={{
-                            height: "10vh",
-                            width: "10vh",
-                          }}
-                        />
-                      )}
-                    ></Spinner>
-                  </Col>
-                  <Col>
-                    <Spinner
-                      spining={spinning}
-                      result={index}
-                      speed={200}
-                      data={answers}
-                      draw={(item) => (
-                        <div
-                          style={{
-                            height: "10vh",
-                            width: "10vh",
-                            lineHeight: "12vh",
-                            textAlign: "center",
-                            color: "orange",
-                            backgroundColor: "aliceblue",
+	return (
+		<div style={{ minHeight: "100%", textAlign: "center" }}>
+			{index > -1 ? (
+				<>
+					<Container>
+						<div className="d-lg-none d-md-none">
+							<div className="d-flex justify-content-center">
+								<Row>
+									<Col>
+										<Spinner
+											spining={spinning}
+											result={answers[index].row}
+											speed={200}
+											data={images}
+											draw={(item) => (
+												<Image
+													fluid
+													src={`${window.location.origin}/assets/rolly/images/${item}`}
+													style={{
+														height: "10vh",
+														width: "10vh",
+													}}
+												/>
+											)}
+										></Spinner>
+									</Col>
+									<Col>
+										<Spinner
+											spining={spinning}
+											result={index}
+											speed={200}
+											data={answers}
+											draw={(item) => (
+												<div
+													style={{
+														height: "10vh",
+														width: "10vh",
+														lineHeight: "12vh",
+														textAlign: "center",
+														color: "orange",
+														backgroundColor: "aliceblue",
 
-                            fontFamily: "Dotted",
-                            fontSize: "3rem",
-                          }}
-                        >
-                          {item.number}
-                        </div>
-                      )}
-                    ></Spinner>
-                  </Col>
-                </Row>
-              </div>
-            </div>
-            <Row>
-              <Col className="left" sm={12} md={6} style={{ margin: "2.4rem" }}>
-                <Ticket data={data} putData={putData} />
-              </Col>
-              <Col sm={12} md={4} style={{ margin: "2.4rem" }}>
-                <Row>
-                  <Jumbotron
-                    className="rules"
-                    style={{ width: "100%", textAlign: "left" }}
-                  >
-                    <h3>Rules</h3>
-                    <p>
-                      <ul>
-                        <li>Rules</li>
-                        <li>Rules</li>
-                        <li>Rules</li>
-                        <li>Rules</li>
-                        <li>Rules</li>
-                      </ul>
-                    </p>
-                  </Jumbotron>
-                </Row>
-                <div className="hide-in-small">
-                  <Row>
-                    <Col>
-                      <Spinner
-                        spining={spinning}
-                        result={answers[index].row}
-                        speed={100}
-                        data={images}
-                        draw={(item) => (
-                          <Image
-                            fluid
-                            src={`${window.location.origin}/assets/rolly/images/${item}`}
-                            style={{
-                              height: "12vh",
-                              width: "100%",
-                            }}
-                          />
-                        )}
-                      ></Spinner>
-                    </Col>
-                    <Col>
-                      <Spinner
-                        spining={spinning}
-                        result={index}
-                        speed={100}
-                        data={answers}
-                        draw={(item) => (
-                          <div
-                            style={{
-                              height: "12vh",
-                              width: "100%",
-                              lineHeight: "12vh",
-                              textAlign: "center",
-                              color: "orange",
-                              backgroundColor: "aliceblue",
+														fontFamily: "Dotted",
+														fontSize: "3rem",
+													}}
+												>
+													{item.number}
+												</div>
+											)}
+										></Spinner>
+									</Col>
+								</Row>
+							</div>
+						</div>
+						<Row>
+							<Col className="left" sm={12} md={6} style={{ margin: "2.4rem" }}>
+								<Ticket data={data} putData={putData} />
+							</Col>
+							<Col sm={12} md={4} style={{ margin: "2.4rem" }}>
+								<Row>
+									<Jumbotron
+										className="rules"
+										style={{ width: "100%", textAlign: "left" }}
+									>
+										<h3>Rules</h3>
+										<p>
+											<ul>
+												<li>Rules</li>
+												<li>Rules</li>
+												<li>Rules</li>
+												<li>Rules</li>
+												<li>Rules</li>
+											</ul>
+										</p>
+									</Jumbotron>
+								</Row>
+								<div className="hide-in-small">
+									<Row>
+										<Col>
+											<Spinner
+												spining={spinning}
+												result={answers[index].row}
+												speed={100}
+												data={images}
+												draw={(item) => (
+													<Image
+														fluid
+														src={`${window.location.origin}/assets/rolly/images/${item}`}
+														style={{
+															height: "12vh",
+															width: "100%",
+														}}
+													/>
+												)}
+											></Spinner>
+										</Col>
+										<Col>
+											<Spinner
+												spining={spinning}
+												result={index}
+												speed={100}
+												data={answers}
+												draw={(item) => (
+													<div
+														style={{
+															height: "12vh",
+															width: "100%",
+															lineHeight: "12vh",
+															textAlign: "center",
+															color: "orange",
+															backgroundColor: "black",
 
-                              fontFamily: "Dotted",
-                              fontSize: "5rem",
-                            }}
-                          >
-                            {item.number}
-                          </div>
-                        )}
-                      ></Spinner>
-                    </Col>
-                  </Row>
-                </div>
-              </Col>
-            </Row>
-          </Container>
-        </>
-      ) : (
-        <h1>loading</h1>
-      )}
-    </div>
-  );
+															fontFamily: "Dotted",
+															fontSize: "5rem",
+														}}
+													>
+														{item.number}
+													</div>
+												)}
+											></Spinner>
+										</Col>
+									</Row>
+								</div>
+							</Col>
+						</Row>
+					</Container>
+				</>
+			) : (
+				<h1>loading</h1>
+			)}
+		</div>
+	);
 }
--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -2,31 +2,27 @@ import React, { useEffect, useState } from "react";
 import "./Spinner.css";
 
 export default function Spinner({ spining, result, data, speed, draw }) {
-  const [index, setIndex] = useState(0);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (spining) {
-        setIndex((index + 1) % data.length);
-      }
-    }, speed);
-    return () => clearInterval(interval);
-  }, [spining, data, index, speed]);
+	const [index, setIndex] = useState(0);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			if (spining) {
+				setIndex((index + 1) % data.length);
+			}
+		}, speed);
+		return () => clearInterval(interval);
+	}, [spining, data, index, speed]);
 
-  return (
-    <div>
-      <div className="led-box">
-        <div className="led-yellow"></div>
-        <p>OK</p>
-      </div>
-      <div>
-        {spining ? (
-          <div className="roll">
-            <div id="blur">{draw(data[index])}</div>
-          </div>
-        ) : (
-          <div className="roll">{draw(data[result])}</div>
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div>
+				{spining ? (
+					<div className="roll">
+						<div id="blur">{draw(data[index])}</div>
+					</div>
+				) : (
+					<div className="roll">{draw(data[result])}</div>
+				)}
+			</div>
+		</div>
+	);
 }
